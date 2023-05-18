e49f12dd12afb9b4ec029ac22b76200005747d76
Ada Salvador <ada.salvador@upc.edu>
2023-05-18 16:57:54 +0200

    pr1

--- main.py
+++ main.py
@@ -2,3 +2,18 @@
 Treball Final: Ada Salvador Avalos i Milene Soledad Granda Becerra
 
 """
+import struct as st
+import OpenCV as cv
+#pip install pillow
+from PIL import Image
+
+def lectura_cabecera_jpg() :
+    ...
+
+
+def lectura_cabecera_png():
+    ...
+
+def principal():
+    ...
+    
