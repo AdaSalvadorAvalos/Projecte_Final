370f130a4526fb596f678ff8fd8311d7032ab134
Ada Salvador <ada.salvador@upc.edu>
2023-05-24 17:40:00 +0200

    main 1

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from tkinter import messagebox
 from tkinter.ttk import Combobox
 #clases hechas para el programa
 from Zoom import ImagenZoom
-from fewfilters import *
+from fewfilters import Fewfilters
 
